0e106a3c923d502105a70ad328d8c69a040bb33e
Reon OHASHI <is211014@if001.cis.local>
2022-07-19 13:09:25 +0900

    入力に対応

--- Recurrence_formula/Recurrence_formula.java
+++ Recurrence_formula/Recurrence_formula.java
@@ -1,10 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 class Recurrence_formula{
     public static void main(String[] args){
-        float first = 1;
-        float ratio= 2;
-        float cons = 1;
+        System.out.println("初項aを入力してください");
+        float first = getFloat();
+        System.out.println("αを入力してください");
+        float ratio= getFloat();
+        System.out.println("βを入力してください");
+        float cons = getFloat();
         float ans = first;
         int i = 1;
         List<Float> l = new ArrayList<Float>(); 
@@ -16,5 +22,16 @@ class Recurrence_formula{
         }
         System.out.println("n="+ans+" 項数="+i);
         System.out.println("すべての項"+l);
+    }
+
+    public static float getFloat(){
+        while(true){
+            try{
+                Scanner sc = new Scanner(System.in);
+                return sc.nextFloat();
+            }catch (InputMismatchException e){
+                System.out.println("数字で入力してください");
+            }
+        }
     }
 }
